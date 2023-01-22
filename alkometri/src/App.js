2f2e2d33386a778c1041d1104c20b7e683494885
Jorma <joel.oksanen1223@gmail.com>
2023-01-22 11:19:46 +0200

    erillinen gender-valinta puuttuu

--- alkometri/src/App.js
+++ alkometri/src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [bottles, setBottles] = useState(0) 
   const [time, setTime] = useState(0) 
   const [result, setResult] = useState(0)
+  
 
   
   const litres = bottles *0.33
@@ -24,10 +25,12 @@ function handleSubmit(e) {
 
 
   const conversion1 = gramsLeft / (weight * 0.7)
+  const conversion2 = gramsLeft / (weight * 0.6)
   
 
-  if (conversion1 < 0) {setResult(0)}
-else {setResult(conversion1)}
+  if (conversion1 , conversion2 < 0) {setResult(0)}
+else {setResult("If the user is male " + conversion1 + ". If the user is female " + conversion2)}
+
 
 }
 
@@ -35,25 +38,75 @@ else {setResult(conversion1)}
   return (
     <form onSubmit = {handleSubmit}>
 
-    <h3>Joelin humalamittari</h3>
+    <h3>The alcohol blood level calculator</h3>
     
-    <label>Weight</label>
-    <input type="number" value={weight} onChange={e => setWeight(e.target.value)}/>
+    
+
+  <div>
+
+      <label>Weight</label>
+    <input type="number" value={weight} onChange={e => setWeight(e.target.value)}/> 
+
+  </div>
+
+  <div>
+
 
     <label>Bottles</label>
-    <input type="number" value={bottles} onChange={e => setBottles(e.target.value)}/>
+    <select type="number" value={bottles} onChange={e => setBottles(e.target.value)}>
+
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+
+    </select>
+
+
+  </div>
+
+
+  <div>
 
     <label>Time</label>
-    <input type="number" value={time} onChange={e => setTime(e.target.value)}/>
+    <select type="number" value={time} onChange={e => setTime(e.target.value)}>
 
-   
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
 
-    <label> Promillemäärä:</label>
+    </select>
+
+
+    </div>
+
+
+    <div>
+
+    <label> Blood alcohol level:</label>
     <output> {result} </output>
+    <button> Calculate</button>
 
-  
+    </div>    
 
-    <button> Laske</button>
+    
     </form>
   )
 }
