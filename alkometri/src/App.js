00de3c405e9f4c02598d3a883913e778c6295216
Jorma <joel.oksanen1223@gmail.com>
2023-01-22 10:01:32 +0200

    1

--- alkometri/src/App.js
+++ alkometri/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+
 
 function App() {
+
+  const [age, setAge ] = useState(0) 
+  const [upper, setUpper] = useState(0) 
+  const [lower, setLower] = useState(0) 
+  
+
+  const result = lower + "-" + upper
+
+function handleSubmit(e) {
+
+
+  e.preventDefault()
+  const conversion1 = (220 - age) * 0.65
+  setLower(conversion1)
+
+  const conversion2 = (220 - age) * 0.85
+  setUpper(conversion2)
+
+  
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit = {handleSubmit}>
+
+    <h3>HRLC</h3>
+    
+    <label>Age</label>
+    <input type="number" value={age} onChange={e => setAge(e.target.value)}/>
+
+    <label> Limits</label>
+    <output> {result} </output>
+
+    <button> Laske</button>
+    </form>
+  )
 }
 
 export default App;
