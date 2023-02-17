2287dd3ccf17242013f179c9db17dedc3d708d58
Jorma <joel.oksanen1223@gmail.com>
2023-02-17 14:44:14 +0200

    testi

--- 2nd_practical_assignment/src/App.js
+++ 2nd_practical_assignment/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
+import './App.css'
+
+const URL = 'https://api.exchangerate.host/latest'
+
 
 function App() {
+
+  const [eur, setEur] = useState(0)
+  const [gbp, setGbp] = useState(0)
+  const [rate, setRate] = useState(0)
+
+
+async function convert(e) {
+
+  e.preventDefault()
+
+  try {
+    const address = URL
+    const response = await fetch(address)
+
+    if (response.ok) {
+
+      const json = await response.json()
+      setRate(json.rates.GBP)
+      setGbp(eur * json.rates.GBP)
+    } else {
+
+      alert('Error retrieving exchange rate!')
+    }
+  } catch (err) {
+    alert(err)
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div id="container">
+      <form onSubmit={convert}>
+        <div>
+          
+          <label>Eur</label>&nbsp;
+          <input type="number" step="0.01" value={eur} onChange={e => setEur(e.target.value)} />
+
+          <output>{rate}</output>
+        </div>
+        <div>
+          <label>Gbp</label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+
+        <div>
+          <button> Calculate </button>
+        </div>
+        </form>
     </div>
-  );
+  )
+    
 }
 
 export default App;
